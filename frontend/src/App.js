0ebf7c70cdd546cc07a19beab72353d4d6f79eff
Benaya A.S <84769509+Benezerds@users.noreply.github.com>
2024-12-08 00:52:39 +0700

    Modified the list in app.js to be in a flex row container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,57 @@
-import { useState, useEffect } from 'react';  // import useEffect
-import ContactList from './components/ContactList';
-import Stats from './components/Stats';
-import './App.css';
-import CustomerList from './components/customer/CustomerList';
-import ItemList from './components/item/ItemList';
+import { useState, useEffect } from "react"; // import useEffect
+import ContactList from "./components/ContactList";
+import Stats from "./components/Stats";
+import "./App.css";
+import CustomerList from "./components/customer/CustomerList";
+import ItemList from "./components/item/ItemList";
 
 function App() {
-    const [contacts, setContacts] = useState([]);
-    const [customers, setCustomers] = useState([]);
-    const [items, setItems] = useState([]);
+  const [contacts, setContacts] = useState([]);
+  const [customers, setCustomers] = useState([]);
+  const [items, setItems] = useState([]);
 
+  //  Fetch Contacts Data
+  useEffect(() => {
+    fetch("http://localhost/api/contacts")
+      .then((response) => response.json())
+      .then((data) => setContacts(data))
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
 
-    //  Fetch Contacts Data
-    useEffect(() => {
-        fetch('http://localhost/api/contacts')
-            .then(response => response.json())
-            .then(data => setContacts(data))
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, []);
+  //  Fetch Customers Data
+  useEffect(() => {
+    fetch("http://localhost/api/customers")
+      .then((response) => response.json())
+      .then((data) => setCustomers(data))
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
 
-    //  Fetch Customers Data
-    useEffect(() => {
-        fetch('http://localhost/api/customers')
-            .then(response => response.json())
-            .then(data=> setCustomers(data))
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, []);
+  //  Fetch Items Data
+  useEffect(() => {
+    fetch("http://localhost/api/items")
+      .then((response) => response.json())
+      .then((data) => setItems(data))
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
 
-    //  Fetch Items Data
-    useEffect(() => {
-        fetch('http://localhost/api/items')
-            .then(response => response.json())
-            .then(data => setItems(data))
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, []);
-
-    return (
-        <div className='page'>
-            <h1>Contactor</h1>
-            <ContactList contacts={contacts} setContacts={setContacts} />
-            <CustomerList customers={customers} setCustomers={setCustomers} />
-            <ItemList items={items} setItems={setItems} />
-            <p>Click a contact to view associated phone numbers</p>
-            <Stats />
-        </div>
-    );
+  return (
+    <div className="page">
+      <h1>Ben's Company Data</h1>
+      <div className="list-flex-container-row">
+        <ContactList contacts={contacts} setContacts={setContacts} />
+        <CustomerList customers={customers} setCustomers={setCustomers} />
+        <ItemList items={items} setItems={setItems} />
+      </div>
+      <p>Click a contact to view associated phone numbers</p>
+      <Stats />
+    </div>
+  );
 }
 
 export default App;
