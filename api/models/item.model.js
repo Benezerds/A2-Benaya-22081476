44d01733049313ad19154fb38693b4821e991edf
Benezer <benzzkrieg@gmail.com>
2024-12-07 11:41:24 +0700

    Implemented CRUD operations for Customer, Order, and Item table Modified index.js to implement the relationships between tables Modied Order get route to accept customerID parameter which will be used to only show orders that is related to the specified customerID

--- api/models/item.model.js
+++ api/models/item.model.js
@@ -1,18 +1,17 @@
 module.exports = (sequelize, Sequelize) => {
-    const Item = sequelize.define("item", {
-      item_id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      item_name: {
-        type: Sequelize.STRING,
-      },
-      item_price: {
-        type: Sequelize.INTEGER,
-      },
-    });
-  
-    return Item;
-  };
-  
+  const Item = sequelize.define("item", {
+    item_id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    item_name: {
+      type: Sequelize.STRING,
+    },
+    item_price: {
+      type: Sequelize.INTEGER,
+    },
+  });
+
+  return Item;
+};
